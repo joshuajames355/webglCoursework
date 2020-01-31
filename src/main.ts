eed489a6756edfdc6fb124ef01ca630c05139483
Josh <joshuajames355@gmail.com>
2020-01-31 22:02:34 +0000

    Added basic canvas.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
 function main()
 {
     var el = document.getElementById("root");
-    if (el != null)
+    if (el == null) return;
+
+    const canvas = document.querySelector("#glCanvas") as HTMLCanvasElement;
+    if (canvas == null)
     {
-        el.innerHTML="Loading...";
+        el.innerHTML="Erorr";
+        return;
     }
+
+    const gl = canvas.getContext("webgl");
+
+    // Only continue if WebGL is available and working
+    if (gl === null) {
+        el.innerHTML = "Unable to initialize WebGL.";
+        return;
+    }
+
+    // Set clear color to black, fully opaque
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    // Clear the color buffer with specified clear color
+    gl.clear(gl.COLOR_BUFFER_BIT);
 }
 
 window.onload = main;
