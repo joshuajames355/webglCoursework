583d17c3058c4c466eb74f427899a98a234cfa0f
Josh <joshuajames355@gmail.com>
2020-01-31 22:02:24 +0000

    Switched from babel to awesome-typescript-loader to get strict build.

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
           test: /\.ts?$/,
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader"
+            loader: "awesome-typescript-loader"
           }
         }
       ]
