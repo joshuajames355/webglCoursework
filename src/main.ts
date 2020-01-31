35b3539f182b632b8bf4eb466728c8092f75ead6
Josh <joshuajames355@gmail.com>
2020-01-31 21:38:22 +0000

    Dev enviroment with typescript, babel and webpack

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+function main()
+{
+    var el = document.getElementById("root");
+    if (el != null)
+    {
+        el.innerHTML="Loading...";
+    }
+}
+
+window.onload = main;
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+module.exports = {
+    entry: "./src/main.ts",
+    devtool: 'eval-source-map',
+    resolve: {
+      extensions: [".ts", ".js"]
+    },
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        }
+      ]
+    }
+  };
