22830c72d2b15b9ae0004942b59e99abfdb5ce95
Josh <joshuajames355@gmail.com>
2020-02-05 02:45:13 +0000

    Slight refactor of main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { GunObject, ButtonObject } from "./simpleMesh";
+import { GunObject, ButtonObject, CubeObject } from "./simpleMesh";
 import { vec3, mat4 } from "gl-matrix";
 import FlyingCamera from "./flyingCamera";
 
@@ -28,13 +28,13 @@ function main()
     }
 
     var game = new Game(gl);
-    game.tick();
 }
 
 class Game
 {
     camera : FlyingCamera;
-    cube : GunObject;
+    gun : GunObject;
+    cube : CubeObject;
     gl : WebGL2RenderingContext;
     button : ButtonObject;
 
@@ -44,37 +44,51 @@ class Game
     {
         this.camera = new FlyingCamera();
         this.button = new ButtonObject(gl);
+        this.gun = new GunObject(gl);
+        this.cube = new CubeObject(gl);
 
-        this.cube = new GunObject(gl);
-        this.cube.move(vec3.fromValues(0, 0, -10 ));
+        this.gun.move(vec3.fromValues(0, 0, -10 ));
+        this.cube.move(vec3.fromValues(4, 4, -15 ));
+        this.button.move(vec3.fromValues(-4, 4, -15));
+        this.button.rotateX(90);
 
         this.gl = gl;
-
         this.lastFrameTime = (new Date()).getTime() / 1000;
+
+        this.onAnimationFrame(); //Starts main loop
     }
 
-
-    tick()
+    onAnimationFrame()
     {
         var time = (new Date()).getTime() / 1000;
         var deltaTime = time - this.lastFrameTime;
         this.lastFrameTime = time;
 
+        this.render();
+        this.tick(deltaTime);
+
+        requestAnimationFrame(this.onAnimationFrame.bind(this));
+    }
+
+    render()
+    {
         var view : mat4 = this.camera.getViewMatrix();
         var projection : mat4 = this.camera.getProjectionMatrix();
 
         this.gl.enable(this.gl.DEPTH_TEST);
         // Set clear color to black, fully opaque
-        this.gl.clearColor(0.0, 0.0, 0.5, 1.0);
+        this.gl.clearColor(0, 0, 0, 1.0);
         // Clear the color buffer with specified clear color
         this.gl.clear(this.gl.COLOR_BUFFER_BIT  | this.gl.DEPTH_BUFFER_BIT);
 
         this.button.render(this.gl, view, projection);
+        this.gun.render(this.gl, view, projection);  
+        this.cube.render(this.gl, view, projection);
+    }
 
-        this.cube.render(this.gl, view, projection);  
-        this.cube.tick(deltaTime);
-
-        requestAnimationFrame(this.tick.bind(this));
+    tick(deltaTime : number)
+    {
+        this.gun.tick(deltaTime);
     }
 }
 
